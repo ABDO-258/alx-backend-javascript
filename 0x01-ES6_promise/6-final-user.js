6fa8bd0670aa0fc8e77db6dfe55e1276fababb1c
karbal abdelhadi <abdoraja2005@gmail.com>
2024-04-24 17:37:37 +0100

    task6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then(([photoData, userData]) => {
-      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
-    })
-    .catch(() => {
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+    const results = [
+      { status: userResult.status, value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason },
+      { status: photoResult.status, value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason },
+    ];
+
+    return results;
+  } catch (error) {
+    // Handle any unexpected errors during the process
+    console.error('Error during profile signup:', error);
+    throw error; // Propagate the error upwards if needed
+  }
 }
