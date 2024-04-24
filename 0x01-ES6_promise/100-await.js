77f17d2b0ba8afed1fa94aa9c0ca2a0ac2a6070d
karbal abdelhadi <abdoraja2005@gmail.com>
2024-04-24 19:09:59 +0100

    task10

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const userPromise = await createUser();
+    const photoPromise = await uploadPhoto();
+    return ({ photo: photoPromise, user: userPromise });
+  } catch (error) {
+    return ({ photo: null, user: null });
+  }
+}
