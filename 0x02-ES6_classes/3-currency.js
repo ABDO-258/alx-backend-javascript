a58871a28943b202868ee774055eea314262821d
karbal abdelhadi <abdoraja2005@gmail.com>
2024-04-25 15:37:35 +0100

    task3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError('Name must be a String');
+    }
+    this._code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a String');
+    }
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    const money = `${this._name} (${this._code}).`;
+    return money;
+  }
+}
