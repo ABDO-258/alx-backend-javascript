ec79747ff80dece1ad5747085e5bdadff714c43d
karbal abdelhadi <abdoraja2005@gmail.com>
2024-04-29 20:29:29 +0100

    task10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
   const groceries = map.forEach((element, key, newMap) => {
     if (element === 1) newMap.set(key, 100);
   });
