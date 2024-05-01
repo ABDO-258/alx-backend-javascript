596704efb11f433b2b337fd4614470b20102dce2
karbal abdelhadi <abdoraja2005@gmail.com>
2024-05-01 19:57:39 +0100

    task5

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,35 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+  workFromHome() { return 'Working from home';}
+  getCoffeeBreak() { return 'Getting a coffee break';}
+  workDirectorTasks() { return 'Getting to director tasks';}
+}
+
+export class Teacher implements DirectorInterface {
+  workFromHome() { return 'Cannot work from home';}
+  getCoffeeBreak() { return 'Cannot have a break';}
+  workDirectorTasks() { return 'Getting to work';}
+}
+
+export function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) return new Teacher();
+  return new Director();
+}
+
+console.log(createEmployee(200));
+Teacher
+console.log(createEmployee(1000));
+Director
+console.log(createEmployee('$500'));
+Director
