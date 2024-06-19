2e305a0d44f162cf0ecd36f4509d3dac7e94f849
karbal abdelhadi <abdoraja2005@gmail.com>
2024-06-19 03:47:37 +0100

    task1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+
+console.log('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  // Trim any extra whitespace/newlines from the input
+  const name = data.trim();
+
+  // Display the user's input
+  console.log(`Your name is: ${name}`);
+
+  // Close the input stream
+  process.stdin.end();
+});
+
+process.stdin.on('end', () => {
+  // Display the closing message
+  console.log('This important software is now closing');
+});
