bb6ef396015dcc08626b773433c03cbd59453248
karbal abdelhadi <abdoraja2005@gmail.com>
2024-08-19 20:34:36 +0100

    reading a file asynchronously

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  try {
+    // Read the file synchronously
+    const data = await fs.readFileSync(path, 'utf-8');
+    // Split the data into lines
+    const lines = data.trim().split('\n');
+    // Skip the header line
+    lines.shift();
+    const fields = {};
+    let NumberOfStudents = 0;
+    // Process each student line
+    for (const line of lines) {
+      if (line.trim() !== '') {
+        const [firstName, , , field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstName);
+        NumberOfStudents += 1;
+      }
+    }
+    console.log(`Number of students: ${NumberOfStudents}`);
+    for (const [field, firstNames] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${firstNames.length}. List: ${firstNames.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
